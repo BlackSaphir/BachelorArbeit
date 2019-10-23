1f274adf22c1fa26895c08d02585ffcaa9a9e345
DESKTOP-UEOK4NE\carst <carsten.dollack@yahoo.de>
2019-10-23 17:58:25 +0200

    [UPDATED] GameInstance Multiplayer Code

--- Source/BachelorArbeit/BA_GameInstance.cpp
+++ Source/BachelorArbeit/BA_GameInstance.cpp
@@ -9,29 +9,20 @@ UBA_GameInstance::UBA_GameInstance(const FObjectInitializer& ObjectInitializer)
 {
 	OnCreateSessionCompleteDelegate = FOnCreateSessionCompleteDelegate::CreateUObject(this, &UBA_GameInstance::OnCreateSessionComplete);
 	OnStartSessionCompleteDelegate = FOnStartSessionCompleteDelegate::CreateUObject(this, &UBA_GameInstance::OnStartOnlineGameComplete);
+	OnFindSessionCompleteDelegate = FOnFindSessionsCompleteDelegate::CreateUObject(this, &UBA_GameInstance::OnFind)
 }
 
 bool UBA_GameInstance::HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLan, bool bIsPresence, int32 MaxNumPlayers)
 {
-	IOnlineSubsystem* const Onlinesub = IOnlineSubsystem::Get();
-
-	if (Onlinesub)
-	{
-		IOnlineSessionPtr Sessions = Onlinesub->GetSessionInterface();
-
-		if (Sessions.IsValid() && UserId.IsValid())
-		{
-			SessionSettings = MakeShareable(new FOnlineSessionSettings());
-			//SessionSettings->bIsLan
-		}
-	}
-	return false;
+	
 }
 
 void UBA_GameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
 {
+
 }
 
 void UBA_GameInstance::OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful)
 {
+
 }
--- Source/BachelorArbeit/BA_GameInstance.h
+++ Source/BachelorArbeit/BA_GameInstance.h
@@ -8,7 +8,7 @@
 #include "BA_GameInstance.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class BACHELORARBEIT_API UBA_GameInstance : public UGameInstance
@@ -17,20 +17,29 @@ class BACHELORARBEIT_API UBA_GameInstance : public UGameInstance
 
 		UBA_GameInstance(const FObjectInitializer& ObjectInitializer);
 
-		bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLan, bool bIsPresence, int32 MaxNumPlayers);
-	
+
 
 	// Delegates
 	FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
 	FOnStartSessionCompleteDelegate OnStartSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate OnFindSessionCompleteDelegate;
+	FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
+	FOnDestroySessionCompleteDelegate OnDestroySessionCompleteDelegate;
+
 	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
 	FDelegateHandle	OnStartSessionCompleteDelegateHandle;
-
-	TSharedPtr<class FOnlineSessionSettings> SessionSettings;
+	FDelegateHandle OnFindSessionCompleteDelegateHandle;
+	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+	FDelegateHandle OnDestroySessionCompeteDelegateHandle;
 
 
 	//Functions
 	virtual void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
 	void OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful);
+
+	//UFUNCTION(BlueprintCallable, Category = "Multiplayer")
+	bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLan, bool bIsPresence, int32 MaxNumPlayers);
+
+	TSharedPtr<class FOnlineSessionSettings> SessionSettings;
 
 };
